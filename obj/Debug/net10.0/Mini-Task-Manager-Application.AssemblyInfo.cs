32b85c306bef5272598a582dfccfc58423cddd5c
Kerry Mannette <kerrymannette2@gmail.com>
2026-02-10 02:16:38 -0400

    Remove remaining build artifacts from tracking

--- obj/Debug/net10.0/Mini-Task-Manager-Application.AssemblyInfo.cs
+++ obj/Debug/net10.0/Mini-Task-Manager-Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3224e97d842e5286b223e332154f11c51a5dbb91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fac39051fb3f72d7cd52e8339ed1f8624cf8aa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
